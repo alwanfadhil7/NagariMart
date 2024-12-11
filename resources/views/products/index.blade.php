9e1289f3d3d4115c10863096aba2cd5a4a304601
alwan <alwanfadhil7@gmail.com>
2024-12-11 14:48:32 +0700

    fix bug

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,19 +2,19 @@
 <html>
 
 <head>
-    <title>Nagari Mart</title>
+    <title>Online Shop</title>
 </head>
 
 <body>
-    <h1>List Produk</h1>
+    <h1>Product List</h1>
     <ul>
         @foreach($products as $product)
         <li>
-            <img src="{{asset('storage/' .$product->image) }}" alt="{{$product->name }}" width="100">
-            <h2>{{$product->name}}</h2>
-            <p>{{$product->description}}</p>
-            <p>{{price: ${{$product->price}}}}</p>
-            <form action="{{route('cart.add', $product->id)}}" method="POST">
+            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" width="100">
+            <h2>{{ $product->name }}</h2>
+            <p>{{ $product->description }}</p>
+            <p>Price: ${{ $product->price }}</p>
+            <form action="{{ route('cart.add', $product->id) }}" method="POST">
                 @csrf
                 <button type="submit">Add to Cart</button>
             </form>
