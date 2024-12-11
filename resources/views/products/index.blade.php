e37feda49353735f480d7b81f6e6250fe93eb772
alwan <alwanfadhil7@gmail.com>
2024-12-11 14:44:36 +0700

    tampilan untuk Product

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Nagari Mart</title>
+</head>
+
+<body>
+    <h1>List Produk</h1>
+    <ul>
+        @foreach($products as $product)
+        <li>
+            <img src="{{asset('storage/' .$product->image) }}" alt="{{$product->name }}" width="100">
+            <h2>{{$product->name}}</h2>
+            <p>{{$product->description}}</p>
+            <p>{{price: ${{$product->price}}}}</p>
+            <form action="{{route('cart.add', $product->id)}}" method="POST">
+                @csrf
+                <button type="submit">Add to Cart</button>
+            </form>
+        </li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
